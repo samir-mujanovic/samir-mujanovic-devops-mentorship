5e602fcdca4a45feb44127eefb6b9c8206045bc9
samir-mujanovic <samir_nox@hotmail.com>
2023-06-07 18:49:00 +0200

    task-12

---
+++ week-15/IaC- Terraform/terraform.tfvars
@@ -0,0 +1,4 @@
+default_cidr_block = "0.0.0.0/0"
+tag_name = "Samir Mujanovic"
+instance_type = "t2.micro"
+ami_id = "ami-0af421fbd663efcac"
---
+++ week-15/Packer/ec2-user-data.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sleep 30
+sudo yum update -y
+sudo rpm --import https://repo.mysql.com/RPM-GPG-KEY-mysql-2022
+wget http://dev.mysql.com/get/mysql57-community-release-el7-8.noarch.rpm
